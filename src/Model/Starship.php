f94351fe903d49802ba034bd49095725d2c86893
pheldu95 <phelps.duncan@gmail.com>
2025-03-18 11:27:58 -0500

    created a backed enum for starship status. chapter 16

--- src/Model/Starship.php
+++ src/Model/Starship.php
@@ -5,53 +5,36 @@ namespace App\Model;
 class Starship
 {
     public function __construct(
-        private int    $id,
+        private int $id,
         private string $name,
         private string $class,
         private string $captain,
-        private string $status,
-    )
-    {
+        private StarshipStatusEnum $status,
+    ) {
     }
 
-    /**
-     * @return int
-     */
     public function getId(): int
     {
         return $this->id;
     }
 
-    /**
-     * @return string
-     */
     public function getName(): string
     {
         return $this->name;
     }
 
-    /**
-     * @return string
-     */
     public function getClass(): string
     {
         return $this->class;
     }
 
-    /**
-     * @return string
-     */
     public function getCaptain(): string
     {
         return $this->captain;
     }
 
-    /**
-     * @return string
-     */
-    public function getStatus(): string
+    public function getStatus(): StarshipStatusEnum
     {
         return $this->status;
     }
-
 }
